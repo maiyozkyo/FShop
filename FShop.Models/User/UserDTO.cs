1d3a13294af91d881eb24120fd6ca9bfd4a7a48f
ekkob <ekkob@BLEU>
2023-10-24 17:33:55 +0700

    USer.isAdmin

--- FShop.Models/User/UserDTO.cs
+++ FShop.Models/User/UserDTO.cs
@@ -16,6 +16,7 @@ namespace FShop.Models.User
         public string Phone { get; set; }
         public string Role { get; set; }
         public bool Active { get; set; }
+        public bool IsAdmin { get; set; }
 
         public static explicit operator UserDTO(UserModel v)
         {
